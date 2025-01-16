0c09894016a39cb7e31b35fda8cd0c182393c0f3
sxntiagoad <salvarezd3@eafit.edu.co>
2025-01-16 13:22:35 -0500

    extract ids of the documents on the db

--- app/repositories/report_repository.py
+++ app/repositories/report_repository.py
@@ -10,30 +10,29 @@ class ReportRepository:
             print("Iniciando get_reports...")
             collection_ref = db.collection(report_filter.collection)
             
-            print(f"Creando filtros con fechas: {report_filter.start_date} - {report_filter.end_date}")
-            
             # Crear filtros individuales
             start_date_filter = FieldFilter('fechaInit', '>=', report_filter.start_date)
             end_date_filter = FieldFilter('fechaInit', '<=', report_filter.end_date)
             
-            print("Creando composite filter...")
             composite_filter = BaseCompositeFilter(
                 StructuredQuery.CompositeFilter.Operator.AND, 
                 [start_date_filter, end_date_filter]
             )
-            print("Composite filter creado")
 
             # Aplicar filtro a la consulta
-            print("Aplicando filtro a la consulta...")
             query = collection_ref.where(filter=composite_filter)
-            print("Filtro aplicado")
-
-            print("Ejecutando get()...")
             docs = query.get()
 
-            results = [doc.to_dict() for doc in docs]
+            # Extraer datos e IDs
+            results = []
+            for doc in docs:
+                data = doc.to_dict()
+                data['doc_id'] = doc.id  # Agregar el ID del documento al diccionario
+                results.append(data)
+            
             print(f"Procesados {len(results)} documentos")
             return results
+            
         except Exception as e:
             print(f"Error en la consulta: {str(e)}")
             print(f"Tipo de error: {type(e)}")
